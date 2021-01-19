08f97ad3235b17873c893b7991d6038adf39ba78
all.silver__ <nno3onn@naver.com>
2021-01-19 14:32:42 +0900

    update

--- 04_public_data/server.js
+++ 04_public_data/server.js
@@ -1,7 +1,64 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.Interface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-app.use(express.static('public'));
-var server = app.listen('3000', function () {
+
+var server = http.listen('3000', () => {
     console.log('Application Running: http://localhost:%d', server.address().port);
 });
+
+var io = require('socket.io')(server); 
+app.use(express.static('public'));
+
+// Read csv file
+var Filedata = fs.readFileSync('number_data.csv', 'utf-8');
+var a = '[';
+lines = Filedata.split('\n');
+for(var i=0; i<lines.length; i++){
+    line = lines[i];    // 한 줄
+    a += '[' + line.slice(0, -1) + '],';
+}
+a += ']';
+fs.writeFileSync('converted.json', (a));
+
+
+io.on('connection', (socket)=> {
+    console.log('connected');
+
+    // io.emit('init', Filedata);
+    io.emit('init', a);
+
+});
+
+
+
+
+// var Filedata = fs.readFileSync('public_data.txt', 'utf-8');
+// lines = Filedata.split('\n');
+
+// var array = [];
+
+// for(var i=0; i<lines.length; i++){
+//     line = lines[i];    // 한 줄
+//     var a = [];
+//     for(var j=0, k=0; j<line.length; j++){
+//         word = line[j]; // 한 글자
+//         if(word == '\t'){
+//             k++
+//         }else{
+//             a[k] += word;
+//         }
+//     }
+//     array.push(a);
+//     // console('log: ', line[12], ', lat: ', line[13]);
+// }
+// console.log(array);
+// fs.writeFileSync('converted.json', (array));
+
+
